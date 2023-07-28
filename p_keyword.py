655040d1350e21fe5fd6c9d6ab534ed439f96856
Heesu Jeong <huissppea@gmail.com>
2023-07-29 03:44:17 +0900

    fix: 빠진 import 부분 추가

--- p_keyword.py
+++ p_keyword.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from googleapiclient.discovery import build
 from isodate import parse_duration
 from googleapiclient.errors import HttpError
